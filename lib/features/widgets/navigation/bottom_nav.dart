9bb828562900b6e73df5148ef03b9b085eaadb0b
asamoah-fred <asamoahgodfred42@gmail.com>
2024-01-06 12:50:28 +0000

    fixed navigation bolt

--- lib/features/widgets/navigation/bottom_nav.dart
+++ lib/features/widgets/navigation/bottom_nav.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -21,7 +19,7 @@ class NavigationMenu extends StatefulWidget {
 class _NavigationMenuState extends State<NavigationMenu> {
   int? _selectedIndex;
 
-  final List<Widget> _widgetOptions =[
+  final List<Widget> _widgetOptions = [
     const ServicesScreen(),
     const CashScreen(),
     const HomeScreen(),
@@ -40,74 +38,111 @@ class _NavigationMenuState extends State<NavigationMenu> {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: const Color(0xFFF8F8F8),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: const Color(0xFFF8F8F8),
-        currentIndex: _selectedIndex!,
-        unselectedItemColor: const Color(0xFF676A75),
-        selectedItemColor: const Color(0xFF676A75),
-        landscapeLayout: BottomNavigationBarLandscapeLayout.spread,
-        unselectedLabelStyle: TextStyle(
-          fontSize: 9.sp,
-          fontWeight: FontWeight.w700,
-        ),
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              AppImages.device,
+      bottomNavigationBar: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: const Color(0xFFF8F8F8),
+            currentIndex: _selectedIndex!,
+            unselectedItemColor: const Color(0xFF676A75),
+            selectedItemColor: const Color(0xFF676A75),
+            landscapeLayout: BottomNavigationBarLandscapeLayout.spread,
+            iconSize: 28,
+            unselectedLabelStyle: TextStyle(
+              fontSize: 9.sp,
+              fontWeight: FontWeight.w700,
             ),
-            label: 'Services',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              AppImages.cash,
+            selectedLabelStyle: TextStyle(
+              fontSize: 9.sp,
+              fontWeight: FontWeight.w700,
             ),
-            label: 'Cash',
+            items: [
+              BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    SvgPicture.asset(
+                      AppImages.device,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
+                label: 'Services',
+              ),
+              BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    SvgPicture.asset(
+                      AppImages.cash,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
+                label: 'Cash',
+              ),
+              const BottomNavigationBarItem(
+                icon: Text(''),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    SvgPicture.asset(
+                      AppImages.add,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
+                label: 'Bundles',
+              ),
+              BottomNavigationBarItem(
+                icon: Column(
+                  children: [
+                    SvgPicture.asset(
+                      AppImages.settings,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ],
+                ),
+                label: 'Settings',
+              ),
+            ],
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
           ),
-          const BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(bottom: 5),
-              child: Image(
+          Positioned(
+            bottom: 10,
+            left: 178,
+            child: InkWell(
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                      builder: ((context) => const NavigationMenu(index: 2)),
+                    ),
+                    (route) => true);
+              },
+              child: const Image(
                 image: AssetImage(AppImages.tobi),
                 width: 56,
                 height: 56,
               ),
             ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              AppImages.add,
-            ),
-            label: 'Bundles',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              AppImages.settings,
-            ),
-            label: 'Settings',
           ),
         ],
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
       ),
       body: _widgetOptions.elementAt(_selectedIndex!),
-      // floatingActionButton: FloatingActionButton(
-      //   backgroundColor: Color.fromRGBO(59, 61, 191, 1),
-      //   isExtended: true,
-      //   onPressed: () {
-      //     Navigator.push(
-      //         context, MaterialPageRoute(builder: ((context) => Add())));
-      //   },
-      //   child: Icon(
-      //     Icons.add,
-      //     size: 40,
-      //   ),
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
